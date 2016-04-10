a30c13fdbc4fbe986e54b016265f732ca68e4073
karachevtsev <karachevtsev@yahoo.com>
2016-04-10 21:49:59 +0300

    Add animation for advantages items

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-    $(".toggle-navigation").click(function() {
+
+    $('.toggle-navigation').click(function() {
         $(this).toggleClass("on");
         $(".navigation").slideToggle();
         return false;
     });
+
+    $('.advantages').waypoint(function() {
+        $('.card__item').each(function(index) {
+            var animateCard = $(this);
+            setInterval(function() {
+                animateCard.removeClass('card__item_rotate-off').addClass('card__item_rotate-on');
+            }, 200*index);
+        });
+        // $('.card__item').removeClass('card__item_rotate-off').addClass('card__item_rotate-on');
+        // console.log('it works')
+    });
+
 });
